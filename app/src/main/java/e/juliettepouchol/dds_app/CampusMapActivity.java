c6c103c262c6750516714c3b239da024c5c3faea
Juliette Pouchol <f002qcs@dartmouth.edu>
2018-05-09 15:39:28 -0400

    added settings, map and dining hall activities

--- app/src/main/java/e/juliettepouchol/dds_app/CampusMapActivity.java
+++ app/src/main/java/e/juliettepouchol/dds_app/CampusMapActivity.java
@@ -1,27 +1,45 @@
 package e.juliettepouchol.dds_app;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
+
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import java.util.Calendar;
+import java.util.Date;
 
-public class CampusMapActivity extends FragmentActivity implements OnMapReadyCallback {
-
+public class CampusMapActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
     private GoogleMap mMap;
+    private LatLng dartmouthCampus = new LatLng(43.703359, -72.288558);
+    private LatLng Collis = new LatLng(43.702668, -72.289846);
+    private LatLng Hop = new LatLng(43.701827, -72.288270);
+    private LatLng Kaf = new LatLng(43.705151,-72.288507 );
+    private LatLng FOCO = new LatLng(43.703036, -72.290836);
+    private LatLng Novack = new LatLng(43.705837, -72.289323);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_campus_map);
+        Button time_tab = findViewById(R.id.time);
+        Date currentTime = Calendar.getInstance().getTime();
+        time_tab.setText(currentTime.toString());
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     }
 
 
@@ -38,12 +56,29 @@ public class CampusMapActivity extends FragmentActivity implements OnMapReadyCal
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng dartmouthCampus = new LatLng(43.703359, -72.288558);
-        mMap.addMarker(new MarkerOptions().position(dartmouthCampus).title("Marker on Dartmouth College"));
+        // Add markers at the different dining halls, change color, move camera
+
+        mMap.addMarker(new MarkerOptions().position(Collis).title("Collis"));
+        mMap.addMarker(new MarkerOptions().position(Hop).title("Hop")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+        mMap.addMarker(new MarkerOptions().position(Kaf).title("Kaf"));
+        mMap.addMarker(new MarkerOptions().position(FOCO).title("FOCO"));
+        mMap.addMarker(new MarkerOptions().position(Novack).title("Novack")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+
+        googleMap.setOnMarkerClickListener(this);
+
         mMap.moveCamera(CameraUpdateFactory.newLatLng(dartmouthCampus));
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(dartmouthCampus, 16.0f));
     }
 
 
+    @Override
+    public boolean onMarkerClick(Marker marker) {
+        Toast.makeText(this, marker.getTitle().toString(), Toast.LENGTH_SHORT).show();
+        marker.showInfoWindow();
+        Intent markerIntent = new Intent(CampusMapActivity.this, DiningHallMenu.class);
+        startActivity(markerIntent);
+        return true;
+    }
 }
---
+++ app/src/main/java/e/juliettepouchol/dds_app/DiningHallMenu.java
@@ -0,0 +1,14 @@
+package e.juliettepouchol.dds_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+
+public class DiningHallMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dining_hall_menu);
+    }
+}
